a793bb9b26ad348ea00de86c40c140338859bdaf
Tan Tran <tan@MacBook-Air.local>
2019-11-29 10:02:41 -0800

    all py/js files

---
+++ webflow_loginsignup.js
@@ -0,0 +1,50 @@
+<!-- UI -->
+<script src="https://cdn.firebase.com/libs/firebaseui/3.5.2/firebaseui.js"></script>
+<link type="text/css" rel="stylesheet" href="https://cdn.firebase.com/libs/firebaseui/3.5.2/firebaseui.css" />
+
+<script>
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+var uiConfig = {
+    signInSuccessUrl: './success.htm',
+    signInOptions: [
+        // Leave the lines as is for the providers you want to offer your users.
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    ],
+}
+
+/*ui.start('#firebaseui-auth-container', {
+  signInOptions: [
+    {
+      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      requireDisplayName: false
+    }
+  ]
+});
+*/
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    var displayName = user.displayName;
+    var email = user.email;
+    var emailVerified = user.emailVerified;
+    var photoURL = user.photoURL;
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+    var providerData = user.providerData;
+    // ...
+    console.log('Test email: ' + email);
+    console.log('Test uid: ' + uid);
+
+  } else {
+    // User is signed out.
+    // ...
+  }
+});
+
+</script>
+
+<!-- https://firebase.google.com/docs/auth/web/start -->
---
+++ webflow_study.js
@@ -0,0 +1,81 @@
+<script>
+
+function update_learning_content(thisObj)
+{
+	// Task 2: using Firestore, retrieve next card and update values in boxes
+	firebase.auth().onAuthStateChanged(function(user) {
+	  if (user) {
+	    console.log('update_learning_content got user: ' + user.uid)
+
+	    var query = db.collection('notes').where('user', '==', user.uid);
+	    query.get()
+	    	.then
+	    	(
+	    		function(querySnapshot) 
+	    		{
+	    			querySnapshot.forEach
+	    			(
+	    				function(doc) 
+	    				{
+	    				console.log(doc.id, ' => ', doc.data());
+	    				}
+	    			);
+	    		}
+	    	)
+	    	.catch
+	    	(
+	    		function(error)
+	    		{
+	    			console.log('Error getting documents: ', error);
+	    		}
+	    	);
+	  } else {
+	    console.log('update_learning_content got user: ' + 'NONE')
+	  }
+	});
+
+    // $.ajax(
+    // 	{
+    // 		url: '../ajax/get_next_card/', 
+    // 		dataType: 'json', 
+    // 		success: function(data) 
+    // 		{ 
+    // 			// do these first so the text is not shown at all
+    // 			$("#showAnswerButton").show();
+    // 			$("#answerButtons").hide();
+    // 			$("#postnoteP").hide();
+
+    // 			// set the new cloze (target) and source text
+    // 			console.log("succeeded: " + data.clozed_text);
+    // 			$("#targetP").html(data.target_full_clozed);
+    // 			$("#sourceP").text(data.native_full);
+    //             $("#prenoteP").text(data.pre_note);
+    //             $("#postnoteP").text(data.post_note);
+
+    //             // For interview
+    //             $("#targetP").html("Why on earth did you stay up so late?!");
+    //             $("#sourceP").text("¿Por qué diablos te quedaste despierto tan tarde?");
+    //             $("#prenoteP").text("Este es un idioma, no un significado literal!");
+    //             $("#postnoteP").text("Aprendimos este idioma en el episodio 5.");
+
+    // 			$("#audioP").html('<a href="' + media_url + data.audio_file + '">Clic en este botón para audio.</a>');
+
+    // 			// ######## JQUERY DATA #######
+    // 			// need to save card id so it can be updated later
+    // 			$("#targetP").data('card_id', data.card_id);
+    // 			console.log('prompt -> card_id is... ' + $("#prompt").data('card_id'))
+    // 		},
+    // 		error: function() { console.log("update_learning_content failed.")}
+    // 	}
+    // )
+}
+
+$(this).ready
+(
+	function() {
+		update_learning_content($(this));
+		console.log('ready.')
+	}
+)
+
+</script>
