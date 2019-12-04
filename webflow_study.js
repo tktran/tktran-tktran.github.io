e9a69743531333139af9391ac45fb38ba83df3ca
Tan Tran <tan@MacBook-Air.local>
2019-12-03 16:15:49 -0800

    ...

--- webflow_study.js
+++ webflow_study.js
@@ -1,101 +1,6 @@
-// function update_learning_content(thisObj)
-// {
-// 	// Task 2: using Firestore, retrieve next card and update values in boxes
-// 	var db = firebase.firestore();
-// 	firebase.auth().onAuthStateChanged(function(user) {
-// 	  if (user) {
-// 	    console.log('update_learning_content got user: ' + user.uid)
+var currentCardId = "NA";
 
-// 	    // var query = db.collection('notes').where('user', '==', user.uid);
-// 	    var query = db.collection('notes').where('user', '==', user.uid).orderBy('spacingLastDue');
-// 	    query.get()
-// 	    	.then
-// 	    	(
-// 	    		function(querySnapshot) 
-// 	    		{
-// 	    			querySnapshot.forEach
-// 	    			(
-// 	    				function(doc) 
-// 	    				{
-// 	    				console.log(doc.id, ' => ', doc.data());
-// 	    				}
-// 	    			);
-// 	    		}
-// 	    	)
-// 	    	.catch
-// 	    	(
-// 	    		function(error)
-// 	    		{
-// 	    			console.log('Error getting documents: ', error);
-// 	    		}
-// 	    	);
-// 	  } else {
-// 	    console.log('update_learning_content got user: ' + 'NONE')
-// 	  }
-// 	});
-
-    // $.ajax(
-    // 	{
-    // 		url: '../ajax/get_next_card/', 
-    // 		dataType: 'json', 
-    // 		success: function(data) 
-    // 		{ 
-    // 			// do these first so the text is not shown at all
-    // 			$("#showAnswerButton").show();
-    // 			$("#answerButtons").hide();
-    // 			$("#postnoteP").hide();
-
-    // 			// set the new cloze (target) and source text
-    // 			console.log("succeeded: " + data.clozed_text);
-    // 			$("#targetP").html(data.target_full_clozed);
-    // 			$("#sourceP").text(data.native_full);
-    //             $("#prenoteP").text(data.pre_note);
-    //             $("#postnoteP").text(data.post_note);
-
-    //             // For interview
-    //             $("#targetP").html("Why on earth did you stay up so late?!");
-    //             $("#sourceP").text("¿Por qué diablos te quedaste despierto tan tarde?");
-    //             $("#prenoteP").text("Este es un idioma, no un significado literal!");
-    //             $("#postnoteP").text("Aprendimos este idioma en el episodio 5.");
-
-    // 			$("#audioP").html('<a href="' + media_url + data.audio_file + '">Clic en este botón para audio.</a>');
-
-    // 			// ######## JQUERY DATA #######
-    // 			// need to save card id so it can be updated later
-    // 			$("#targetP").data('card_id', data.card_id);
-    // 			console.log('prompt -> card_id is... ' + $("#prompt").data('card_id'))
-    // 		},
-    // 		error: function() { console.log("update_learning_content failed.")}
-    // 	}
-    // )
-// }
-
-// https://codelabs.developers.google.com/codelabs/firestore-web/#6
-// In this section, you'll learn how to retrieve data from Cloud 
-// Firestore and display it in your app. The two key steps are 
-// creating a query and adding a snapshot listener. This listener 
-// will be notified of all existing data that matches the query 
-// and will receive updates in real time. [Have to do it this way.
-// Can't just get the list of cards all at once, because failed cards
-// need to be able to pop up again]
-
-// getDocumentsInCardQuery = function(query, renderer)
-// {
-// 	query.onSnapshot(function(snapshot) {
-// 		if (!snapshot.size) return;
-
-// 		snapshot.docChanges().forEach(function(change) {
-// 			if (change.type == 'removed') {
-// 				renderer.remove(change.doc);
-// 			} else {
-// 				renderer.display(change.doc);
-// 			}
-// 		});
-// 	});
-// };
-
-
-getDocumentsInCardQuery = function(thisObj)
+attachCardSnapshotListener = function(thisObj)
 {
 	var db = firebase.firestore();
 
@@ -116,6 +21,8 @@ getDocumentsInCardQuery = function(thisObj)
 				}
 				snapshot.forEach(function(doc) {
 					console.log(doc.id, ' -> ', doc.data());
+					currentCardId = doc.id;
+
 					$("#clozedContent").html(doc.get('contentClozed'));
 					$("#nativeTranslation").html(doc.get('contentNativeTranslation'));
 					$("#preNote").html(doc.get('notePre'));
@@ -168,6 +75,10 @@ $("#buttonBest").click
 	}
 )
 
+function setLearningDifficulty(difficulty) {
+	console.log('About to set card w/ id', currentCardId, ' to difficulty ' difficulty);
+}
+
 // active_tf: true
 // clozeNum: 1
 // contentClozed: "政府的[decision]引起了很多不同的指責。"
@@ -183,11 +94,10 @@ $("#buttonBest").click
 // user: "S4BpEFgxflezIHhuzUH19819mg93"
 // __proto__: Object
 
-
 $(this).ready
 (
 	function() {
-		getDocumentsInCardQuery($(this));
+		attachCardSnapshotListener($(this));
 		console.log('Done.')
 	}
 )
