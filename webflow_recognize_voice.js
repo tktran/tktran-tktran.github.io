6fb77641c8f980015128ffb21dd94bdb4ef42084
Tan Tran <tankimtran@gmail.com>
2020-03-29 15:03:38 -0400

    text

--- webflow_recognize_voice.js
+++ webflow_recognize_voice.js
@@ -1,4 +1,7 @@
 $(this).ready( function() {
+	$("#recognizedTextField").text( "一二三你好中文" );
+	data_tables_init("#recognizedTextField");
+
 	// https://codeburst.io/html5-speech-recognition-api-670846a50e92
 	window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
 	if ('SpeechRecognition' in window) {
@@ -14,26 +17,24 @@ $(this).ready( function() {
 		recognition.onresult = (event) => {
 			console.log('recognition onResult event.');
 
-			$("#recognizedTextField").text( "一二三你好中文" );
-			data_tables_init("#recognizedTextField");
-			// let interimTranscript = '';
-			// for (let i = event.resultIndex, len = event.results.length; i < len; i++) 
-			// {
-			// 	let transcript = event.results[i][0].transcript;
-			// 	if (event.results[i].isFinal) {
-			// 		final_transcript += transcript;
-			// 		console.log('Final transcript: ', final_transcript);
-			// 		$("#recognizedTextField").text( 'Final: ' + final_transcript );
+			let interimTranscript = '';
+			for (let i = event.resultIndex, len = event.results.length; i < len; i++) 
+			{
+				let transcript = event.results[i][0].transcript;
+				if (event.results[i].isFinal) {
+					final_transcript += transcript;
+					console.log('Final transcript: ', final_transcript);
+					$("#recognizedTextField").text( 'Final: ' + final_transcript );
 
-			// 		// Should run the datatables init now
-			// 		data_tables_init("#recognizedTextField");
-			// 	} else {
-			// 		interimTranscript += transcript;
-			// 		console.log('Interim transcript: ', interimTranscript);
-			// 		$("#recognizedTextField").text( 'Interim: ' + interimTranscript );
+					// Should run the datatables init now
+					data_tables_init("#recognizedTextField");
+				} else {
+					interimTranscript += transcript;
+					console.log('Interim transcript: ', interimTranscript);
+					$("#recognizedTextField").text( 'Interim: ' + interimTranscript );
 
-			// 	}
-			// }
+				}
+			}
 		}
 		
 		recognition.start();
